f1fadebd4c9b039bf33d29600fc211cca28f6b05
yboyer <bhyw180@gmail.com>
2018-03-13 13:36:52 +0100

    Move db file to ./dbs/

--- users/db.js
+++ users/db.js
@@ -1,2 +1,2 @@
 const Datastore = require('nedb');
-module.exports = new Datastore({filename: `${__dirname}/db`, autoload: true});
+module.exports = new Datastore({filename: `${__dirname}/dbs/db`, autoload: true});
